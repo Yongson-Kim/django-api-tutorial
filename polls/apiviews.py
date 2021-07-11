b588c4104561095ff6064097a056a5ef32e762e0
김용선 <yongson.kim@vntgcorp.com>
2021-07-12 08:07:10 +0900

    Views and Generic Views

--- polls/apiviews.py
+++ polls/apiviews.py
@@ -30,3 +30,11 @@ class PollDetail(generics.RetrieveDestroyAPIView):
     serializer_class = PollSerializer
 
 
+class ChoiceList(generics.ListCreateAPIView):
+    queryset = Choice.objects.all()
+    serializer_class = ChoiceSerializer
+
+
+class CreateVote(generics.CreateAPIView):
+    serializer_class = VoteSerializer
+
--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 # from .views import polls_list, polls_detail
 from .apiviews import PollList, PollDetail
+from .apiviews import ChoiceList, CreateVote
 
 
 urlpatterns = [
     path("polls/", PollList.as_view(), name="polls_list"),
-    path("polls/<int:pk>/", PollDetail.as_view(), name="polls_detail")
+    path("polls/<int:pk>/", PollDetail.as_view(), name="polls_detail"),
+
+    path("choices/", ChoiceList.as_view(), name="choice_list"),
+    path("vote/", CreateVote.as_view(), name="create_vote")
 ]
