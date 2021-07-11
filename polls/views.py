13a2e596902c9cf78c93e21c552b98edc58488a1
김용선 <yongson.kim@vntgcorp.com>
2021-07-12 08:47:24 +0900

    More views and viewsets

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
 
-from polls.models import Poll
-
 
 # Create your views here.
 def polls_list(request):
